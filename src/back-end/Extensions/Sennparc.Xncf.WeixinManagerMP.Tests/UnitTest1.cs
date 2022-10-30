4652897d8d4ed0a4ec8a24772514db84ac4287a1
ConanYao <JaneConan@live.com>
2022-10-30 14:06:14 +0800

    调整参数，使项目可运行

--- src/back-end/Extensions/Sennparc.Xncf.WeixinManagerMP.Tests/UnitTest1.cs
+++ src/back-end/Extensions/Sennparc.Xncf.WeixinManagerMP.Tests/UnitTest1.cs
@@ -9,15 +9,15 @@ namespace Sennparc.Xncf.WeixinManagerMP.Tests
         {
 
 
-            string strname = "YAOSHENGWEI"; // "CHENYUHUI"; YAOSHENGWEI
-            string S1 = strname + "2425";
-            string S2 = CO2NET.   "1FAEDFC98A64D1016E30AF2FB904930D";//UserMd5(S1);
-            string strS2 = S2.Substring(S2.Length - 1);
+            //string strname = "YAOSHENGWEI"; // "CHENYUHUI"; YAOSHENGWEI
+            //string S1 = strname + "2425";
+            //string S2 = CO2NET.   "1FAEDFC98A64D1016E30AF2FB904930D";//UserMd5(S1);
+            //string strS2 = S2.Substring(S2.Length - 1);
 
-            int I1 = Convert.ToInt32(strS2, 16);//int.Parse(strS2);
-            int I2 = I1 + 5;
-            int I3 = I2 % 5;
-            int I4 = I3 + 1;
+            //int I1 = Convert.ToInt32(strS2, 16);//int.Parse(strS2);
+            //int I2 = I1 + 5;
+            //int I3 = I2 % 5;
+            //int I4 = I3 + 1;
         }
     }
 }
